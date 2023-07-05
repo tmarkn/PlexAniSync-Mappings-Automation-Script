7990ff806a35395a8d649868976f1f550fbd069a
Tmarkn <tonymark1997@gmail.com>
2023-07-05 19:57:28 -0400

    Updated Script for better functionality:
     - added additional input formats (ie. "123456" and "anime/123456" (no slash)
     - reduced amount of regex expressions
     - added functionality for seasons split into mulitple parts (still requires user input for where it starts)
     - increased threshold for fuzzy match (to prevent other languages from appearing)

--- getAni.py
+++ getAni.py
@@ -5,16 +5,15 @@ from bs4 import BeautifulSoup as bs
 from thefuzz import fuzz
 from urllib import parse
 
-anilistExp = re.compile(r'\/anime\/([0-9]+)', re.IGNORECASE)
-seasonExp1 = re.compile(r'([0-9]+)(?:st|nd|rd|th) Season', re.IGNORECASE)
-seasonExp2 = re.compile(r'Season ([0-9]+)', re.IGNORECASE)
+anilistExp = re.compile(r'\/?anime\/([0-9]+)', re.IGNORECASE)
+seasonExp = re.compile(r'(?:([0-9]+)(?:st|nd|rd|th) Season)|(?:Season ([0-9]+))|(?:Part ([0-9])+)|(?:Cour ([0-9]+))', re.IGNORECASE)
 romajiExp = re.compile(r'([aeiou]|[bkstgzdnhpmr]{1,2}[aeiou]|(?:sh|ch|j|ts|f|y|w|k)(?:y[auo]|[aeiou])|n|\W|[0-9])+', re.IGNORECASE)
 
 class anilistEntry:
     def __init__(self, title: str) -> None:
         self.title = title
         self.synonyms = []
-        self.seasons = dict()
+        self.seasons = []
     def __repr__(self) -> str:
         string = f'  - title: {json.dumps(self.title)}\n'
         if len(self.synonyms):
@@ -23,22 +22,25 @@ class anilistEntry:
                 string += f'      - {json.dumps(syn)}\n'
         string += '    seasons:\n'
         for season in self.seasons:
-            string += f'      - season: {season}\n'
-            string += f'        anilist-id: {self.seasons[season]}\n'
+            string += f'      - season: {season[0]}\n'
+            string += f'        anilist-id: {season[1]}\n'
         return string
 
+def first(iterable, func=lambda L: L is not None, **kwargs):
+    it = (el for el in iterable if func(el))
+    if 'default' in kwargs:
+        return next(it, kwargs['default'])
+    return next(it) # no default so raise `StopIteration`
 
 def getAniData(url: str, getPrequel: bool = False) -> anilistEntry:
     season = 1
     anilistId = None
     ## check valid link
     parsed_uri = parse.urlparse(url)
-    ## check url for /anime/######/
     
-    if url.startswith('/anime/'):
-        search = anilistExp.search(url)
-        if search is not None:
-            anilistId = int(search.group(1))
+    ## check url for ######
+    if url.isnumeric():
+        anilistId = int(url)
     ## check url for anilist.co/anime/######
     elif parsed_uri.netloc == 'anilist.co':
         splitPath = parsed_uri.path[1:].split('/')
@@ -48,9 +50,13 @@ def getAniData(url: str, getPrequel: bool = False) -> anilistEntry:
         if not splitPath[1].isnumeric():
             raise Exception('provided url does not contain an anime entry')
         anilistId = int(splitPath[1])
-
-    ## not valid url
+    ## check url for /anime/######/
     else:
+        anilistExpSearch = anilistExp.search(url)
+        if anilistExpSearch is not None:
+            anilistId = int(anilistExpSearch.group(1))
+    ## not valid url
+    if not anilistId:
         raise Exception('provided url is not of anilist.co')
     
     ## new url
@@ -78,18 +84,12 @@ def getAniData(url: str, getPrequel: bool = False) -> anilistEntry:
         engName = romajiName
 
     ## check for season number in title
-    search = seasonExp1.search(engName)
+    search = seasonExp.search(engName)
     if search is not None:
-        season = max(int(search.group(1)), season)
-    search2 = seasonExp2.search(engName)
-    if search2 is not None:
-        season = max(int(search2.group(1)), season)
-    search = seasonExp1.search(romajiName)
+        season = max(int(first(search.groups())), season)
+    search = seasonExp.search(romajiName)
     if search is not None:
-        season = max(int(search.group(1)), season)
-    search2 = seasonExp2.search(romajiName)
-    if search2 is not None:
-        season = max(int(search2.group(1)), season)
+        season = max(int(first(search.groups())), season)
 
     ## create new entry 
     alEntry = anilistEntry(engName)
@@ -102,23 +102,20 @@ def getAniData(url: str, getPrequel: bool = False) -> anilistEntry:
         for syn in synList:
             synTitle = syn.text.strip()
             # check fuzzy match or romaji
-            if fuzz.ratio(synTitle, engName) > 30 or fuzz.ratio(synTitle, romajiName) > 30 or romajiExp.fullmatch(synTitle):
+            if fuzz.ratio(synTitle, engName) > 50 or fuzz.ratio(synTitle, romajiName) > 50 or romajiExp.fullmatch(synTitle):
                 alEntry.synonyms.append(synTitle)
             
             # check for season number in synonyms
-            search = seasonExp1.search(synTitle)
+            search = seasonExp.search(synTitle)
             if search is not None:
-                season = max(int(search.group(1)), season)
-            search2 = seasonExp2.search(synTitle)
-            if search2 is not None:
-                season = max(int(search2.group(1)), season)
+                season = max(int(first(search.groups())), season)
 
     ## add Romaji as synonym
     ## check if different from English name
     if romajiName != engName:
         alEntry.synonyms.append(romajiName)
 
-    alEntry.seasons[season] = anilistId
+    alEntry.seasons.append((season, anilistId))
 
     ## recursively find the prequels until the first season
     if getPrequel == True:
@@ -128,7 +125,7 @@ def getAniData(url: str, getPrequel: bool = False) -> anilistEntry:
             fullLink = 'https://anilist.co' + link
             ## get prequel entry and append
             newAlEntry = getAniData(url=fullLink, getPrequel=True) 
-            newAlEntry.seasons.update(alEntry.seasons)
+            newAlEntry.seasons.extend(alEntry.seasons)
 
             return newAlEntry
 
